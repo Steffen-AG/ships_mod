a339bf1378313d13828978716156401a289e04d9
Steffen-AG <steffenag@appstate.edu>
2023-02-13 21:25:57 -0500

    block registries set up

--- src/main/java/com/steffen/testmod/testmod.java
+++ src/main/java/com/steffen/testmod/testmod.java
@@ -1,13 +1,25 @@
 package com.steffen.testmod;
 
+
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import block.ModBlocks;
+import item.ModCreativeModeTabs;
+import item.ModItems;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.Material;
-import net.minecraft.world.level.material.MaterialColor;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 
 @Mod(testmod.MODID)
@@ -15,20 +27,62 @@ import net.minecraftforge.fml.common.Mod;
 public class testmod {
 	
 	public static final String MODID = "testmod";
-	public static Block TestBlock;
+	private static final Logger LOGGER = LogUtils.getLogger();
 	
-	public static void init() {
-		TestBlock = new Block(BlockBehaviour.Properties.of(Material.STONE).color(MaterialColor.COLOR_BLACK));
+	public testmod() {
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		
+		ModItems.register(modEventBus);
+		ModBlocks.register(modEventBus);
+		
+		modEventBus.addListener(this::commonSetup);
+		MinecraftForge.EVENT_BUS.register(this);
+		
+		modEventBus.addListener(this::addCreative);
 	}
-	public void buildContents(CreativeModeTabEvent.BuildContents event) {
-		if (event.getTab() == CreativeModeTabs.INGREDIENTS) {
-			event.accept(TestBlock);
+	private void commonSetup(final FMLCommonSetupEvent event) {
+		
+	}
+	private void addCreative(CreativeModeTabEvent.BuildContents event) {
+		if(event.getTab() == CreativeModeTabs.INGREDIENTS) {
+			event.accept(ModItems.BLACK_OPAL);
+			event.accept(ModItems.RAW_BLACK_OPAL);
+		}
+		if(event.getTab() == CreativeModeTabs.BUILDING_BLOCKS) {
+			event.accept(ModBlocks.BLACK_OPAL_BLOCK_OBJECT);
+			event.accept(ModBlocks.BLACK_OPAL_ORE);
+			event.accept(ModBlocks.DEEPSLATE_BLACK_OPAL_ORE);
+			event.accept(ModBlocks.ENDSTONE_BLACK_OPAL_ORE);
+			event.accept(ModBlocks.NETHERRACK_BLACK_OPAL_ORE);
+		}
+		if(event.getTab() == CreativeModeTabs.NATURAL_BLOCKS) {
+			event.accept(ModBlocks.BLACK_OPAL_ORE);
+			event.accept(ModBlocks.DEEPSLATE_BLACK_OPAL_ORE);
+			event.accept(ModBlocks.ENDSTONE_BLACK_OPAL_ORE);
+			event.accept(ModBlocks.NETHERRACK_BLACK_OPAL_ORE);
+		}
+		if(event.getTab() == ModCreativeModeTabs.TUTORIAL_TAB) {
+			event.accept(ModItems.BLACK_OPAL);
+			event.accept(ModItems.RAW_BLACK_OPAL);
+			event.accept(ModBlocks.BLACK_OPAL_BLOCK_OBJECT);
+			event.accept(ModBlocks.BLACK_OPAL_ORE);
+			event.accept(ModBlocks.DEEPSLATE_BLACK_OPAL_ORE);
+			event.accept(ModBlocks.ENDSTONE_BLACK_OPAL_ORE);
+			event.accept(ModBlocks.NETHERRACK_BLACK_OPAL_ORE);
 		}
 	}
 	
-	public testmod() {
-		MinecraftForge.EVENT_BUS.register(this);
-		MinecraftForge.EVENT_BUS.register(TestBlock);
+	@Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+	public static class ClientModEvents {
+		@SubscribeEvent
+		public static void onClientSetup(FMLClientSetupEvent event) {
+			
+		}
 	}
+	
+	
+	
 
 }
+
+
---
+++ src/main/java/item/ModCreativeModeTabs.java
@@ -0,0 +1,24 @@
+package item;
+
+import com.steffen.testmod.testmod;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = testmod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ModCreativeModeTabs {
+	public static CreativeModeTab TUTORIAL_TAB;
+	
+	@SubscribeEvent
+	public static void registerCreativeModeTabs(CreativeModeTabEvent.Register event) {
+		TUTORIAL_TAB = event.registerCreativeModeTab(new ResourceLocation(testmod.MODID, "tutroial_tab"),
+				builder -> builder.icon(() -> new ItemStack(ModItems.BLACK_OPAL.get()))
+				.title(Component.translatable("creativemodetab.tutorial_tab")));
+	}
+
+}
