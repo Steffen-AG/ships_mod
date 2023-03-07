7851e56cc5884e3c59f231c8cd57609bcc80ac34
Steffen-AG <steffenag@appstate.edu>
2023-03-07 14:03:32 -0500

    idk

--- src/main/java/item/blackOpal.java
+++ src/main/java/item/blackOpal.java
@@ -1,5 +1,19 @@
 package item;
 
-public class blackOpal {
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+
+public class blackOpal extends Item {
+	public blackOpal(Properties properties){
+		super(properties);
+	}
+	@Override
+	public InteractionResultHolder<ItemStack> use(Level world, Player player, InteractionHand hand){
+		return super.use(world, player, hand);
+	}
 
 }
